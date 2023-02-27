3b0838021504052c0d1214c40655e1363ccd6ef9
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2023-02-27 02:50:13 +0100

    Exercise 3: Passing the color value with props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import ProfileCard from './ProfileCard';
+import React from 'react';
+import BrightText from './BrightText'
 
 function App() {
-  return (
-    <div>
-      <div>Personal Digital Assistants</div>
-
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
-    </div>
-  );
+    return (
+        <div>
+          <BrightText color="red" />
+          <BrightText color="green" />
+          <BrightText color="blue" />
+        </div>
+    );
 }
 
 export default App;
